3897a724766f49f339eaef934eaae3cd47cd5b7e
Shuvapandit <chakrabortyshuva216@gmail.com>
2022-05-23 02:14:00 +0600

    Home page banner part update

--- src/pages/Home/Product/Product.js
+++ src/pages/Home/Product/Product.js
@@ -10,10 +10,10 @@ const{name,img,description,price,quantity}=product;
 
  <div class="card w-25 bg-base-100 shadow-xl">
   <figure><img className='w-50 igm' src={img} alt="products" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">{name}</h2>
-    <h2>Price:{price} Taka</h2>
-    <h2>Quantity:{quantity}</h2>
+  <div className='card-body'>
+    <h2 className="card-title">{name}</h2>
+    <h2 > <span className=' font-bold'> Price: </span>{price} Taka</h2>
+    <h2> <span className=' font-bold'> Quantity: </span> {quantity}</h2>
     <p>{description}</p>
     <div class="card-actions justify-end">
       <button class="btn btn-primary">Purchase</button>
